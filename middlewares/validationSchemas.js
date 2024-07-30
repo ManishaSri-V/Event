ee4450f833b821ba9ccb1fbeb1aaccca09351cd2
ManishaSri-V <148282862+ManishaSri-V@users.noreply.github.com>
2024-07-30 18:45:43 +0530

    Delete middlewares directory

--- middlewares/validationSchemas.js
+++
@@ -1,8 +0,0 @@
-const Joi = require("joi");
-
-const createEventSchema = Joi.object({
-  title: Joi.string().min(8).max(60).required(),
-  date: Joi.date().required(),
-});
-
-module.exports = { createEventSchema };
